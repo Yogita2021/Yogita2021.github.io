ce8216196c19bc034b226ca8935a83bdde3407a3
Yogita2021 <maneyogita2021@gmail.com>
2023-05-16 15:17:59 +0530

    portfolio first draft

---
+++ script.js
@@ -0,0 +1,42 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+
+document.getElementById("resume-link-1").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/10djl9GM1DbYpvoowgRtHdNLv5BSx7Gsb/view?usp=share_link",
+    "_blank"
+  );
+});
+
+document.getElementById("resume-link-2").addEventListener("click", () => {
+  // console.log("OPENinig.....");
+  window.open(
+    "https://drive.google.com/file/d/10djl9GM1DbYpvoowgRtHdNLv5BSx7Gsb/view?usp=share_link",
+    "_blank"
+  );
+});
